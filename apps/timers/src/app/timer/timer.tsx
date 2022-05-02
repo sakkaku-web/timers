ae5db79c65cd9c07055bd14fb620acc94027919b
myin <minyin142p@gmail.com>
2022-05-02 17:00:29 +0200

    feat: add start date

--- apps/timers/src/app/timer/timer.tsx
+++ apps/timers/src/app/timer/timer.tsx
@@ -1,4 +1,4 @@
-import { add, Duration, intervalToDuration } from 'date-fns';
+import { add, Duration, format, intervalToDuration, isValid } from 'date-fns';
 import { useCallback, useEffect, useState } from 'react';
 import { IoMdPause, IoMdPlay, IoMdRefresh, IoMdTrash } from 'react-icons/io';
 import './timer.module.scss';
@@ -21,16 +21,34 @@ const prefixZero = (num?: number): string => {
 };
 
 const TIMER_KEY_PREFIX = 'sakkaku-web-timers-timer-';
+const TIMER_START_KEY_PREFIX = 'sakkaku-web-timers-timerStart-';
 
 export function Timer({ name, onDelete }: TimerProps) {
   const saveKey = TIMER_KEY_PREFIX + name;
+  const startTimeKey = TIMER_START_KEY_PREFIX + name;
+
   const loadSavedTime = (): number => {
     const time = parseInt(localStorage.getItem(saveKey) || '0');
     return isNaN(time) ? 0 : time;
   };
 
+  const loadStartTime = (): Date | null => {
+    const time = new Date(localStorage.getItem(startTimeKey) || '');
+    return isValid(time) ? time : null;
+  };
+
   const [paused, setPaused] = useState(true);
   const [timeInSec, setTimeInSec] = useState(loadSavedTime());
+  const [startTime, setStartTime] = useState(loadStartTime());
+
+  const updateStartTime = (time: Date | null) => {
+    setStartTime(time);
+    if (time) {
+      localStorage.setItem(startTimeKey, time.toISOString());
+    } else {
+      localStorage.removeItem(startTimeKey);
+    }
+  };
 
   const updatePaused = (paused: boolean) => {
     setPaused(paused);
@@ -39,6 +57,7 @@ export function Timer({ name, onDelete }: TimerProps) {
   const resetTime = () => {
     setPaused(true);
     setTimeInSec(0);
+    updateStartTime(null);
   };
 
   const saveTime = useCallback(() => {
@@ -65,6 +84,9 @@ export function Timer({ name, onDelete }: TimerProps) {
     };
 
     if (!paused) {
+      if (timeInSec === 0) {
+        updateStartTime(new Date());
+      }
       interval = setInterval(() => increaseTime(), 1000);
     } else {
       clear();
@@ -83,7 +105,14 @@ export function Timer({ name, onDelete }: TimerProps) {
 
   return (
     <div className="flex flex-col items-center gap-2">
-      <h2 className="font-bold text-3xl">{name}</h2>
+      <h2 className="font-bold text-3xl flex flex-col">
+        {name}
+        {startTime && (
+          <span className="text-xs text-slate-500">
+            {format(startTime, 'dd.MM.yyyy')}
+          </span>
+        )}
+      </h2>
       <span>{durationStr}</span>
       <div className="flex flex-row gap-1">
         {!paused && (
